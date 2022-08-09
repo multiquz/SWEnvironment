9b862236db3e6c0e24ddbff3e38763eed459661a
Vladimir <vproshchin@gmail.com>
2022-08-09 12:42:50 +0300

    Initial Commit

---
+++ SWEnvironment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SWEnvironment
+//
+//  Created by user on 09.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
